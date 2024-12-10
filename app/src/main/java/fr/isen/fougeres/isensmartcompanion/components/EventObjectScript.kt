62413267f6ca77879c9821151c0b8d46871cf9ee
Noah Fougeres <Noah.fougeres310@gmail.com>
2024-12-10 22:58:01 +0100

    ISENSmartCompanion : Added the final things that needed to be added.

--- app/src/main/java/fr/isen/fougeres/isensmartcompanion/components/EventObjectScript.kt
+++ app/src/main/java/fr/isen/fougeres/isensmartcompanion/components/EventObjectScript.kt
@@ -13,9 +13,8 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import fr.isen.fougeres.isensmartcompanion.screens.EventDetailActivity
 import fr.isen.fougeres.isensmartcompanion.backgroundColor
+import fr.isen.fougeres.isensmartcompanion.screens.EventDetailActivity
 import java.io.Serializable
 
 var eventsList = emptyList<EventObject>()
